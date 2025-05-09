99827347cd0ba7f0d31386995951d5e2f1ee332e
chaouch442 <chaouchrimel@gmail.com>
2025-05-09 12:30:10 +0200

    Ajout de nouvelles fonctionnalités / corrections

--- src/reservations/meal-time/meal-time.controller.ts
+++ src/reservations/meal-time/meal-time.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Param, Body, ParseUUIDPipe, Patch, Delete, NotFoundException } from '@nestjs/common';
 import { MealTimeService } from './meal-time.service';
 import { MealTimeEntity } from 'src/plats/entities/meal-time.entity';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('meal-times')
 export class MealTimeController {
@@ -9,22 +10,24 @@ export class MealTimeController {
     async getMealTimeCounts() {
         return this.mealTimeService.countByMealTime();
     }
+    @Roles('manager', 'Customer', 'serveur')
     @Get()
     findAll() {
         return this.mealTimeService.findAll();
     }
-
+    @Roles('manager', 'Customer', 'serveur')
     @Get(':id')
     getMealTimeById(@Param('id', ParseUUIDPipe) id: string) {
         return this.mealTimeService.getMealTimeById(id);
     }
-
+    @Roles('manager')
     @Post()
     createMealTime(
         @Body() mealTimeData: Partial<MealTimeEntity> & { restaurantId: string }
     ) {
         return this.mealTimeService.createMealTime(mealTimeData);
     }
+    @Roles('manager')
     @Patch(':id')
     updateMealTime(
         @Param('id') id: string,
@@ -32,6 +35,7 @@ export class MealTimeController {
     ) {
         return this.mealTimeService.updateMealTime(id, updateData);
     }
+    @Roles('manager')
     @Delete(':mealId')
     async deleteMealTime(@Param('mealId') mealId: string) {
         return this.mealTimeService.deleteMealTime(mealId);
